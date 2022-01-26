f890c88a1ae195607c81f9c57fd72c623491549f
RomanKrivocheev <roman.krivocheev@gmail.com>
2022-01-25 23:11:56 -0300

    lint added, basic filter function added. Clear status w/o reloading added

--- rampup/src/App.js
+++ rampup/src/App.js
@@ -1,67 +1,92 @@
-import React, {useState} from 'react';
-import './App.css';
-import data from './data.json';
+import React, { useState } from "react";
+import "./App.css";
+import data from "./data.json";
 
 import Header from "./Header";
 import ToDoList from "./ToDoList";
-import ToDoForm from './ToDoForm';
+import ToDoForm from "./ToDoForm";
+
+let filtering;
 
 function App() {
-  
-  if(window.localStorage.getItem('user')) {
-    data = JSON.parse(window.localStorage.getItem('user'));
+  if (window.localStorage.getItem("user")) {
+    data = JSON.parse(window.localStorage.getItem("user"));
   } else {
   }
-  
-  const [ toDoList, setToDoList ] = useState(data);
+
+  const [toDoList, setToDoList] = useState(data);
 
   const handleToggle = (id) => {
-    let mapped = toDoList.map(task => {
-      return task.id === Number(id) ? { ...task, complete: !task.complete } : { ...task};
+    let mapped = toDoList.map((task) => {
+      return task.id === Number(id)
+        ? { ...task, complete: !task.complete }
+        : { ...task };
     });
     setToDoList(mapped);
-  }
+  };
 
   const handleFilter = () => {
-    let filtered = toDoList.filter(task => {
+    let filtered = toDoList.filter((task) => {
       return !task.complete;
     });
     setToDoList(filtered);
-  }
+  };
 
-  const addTask = (userInput ) => {
+  const addTask = (userInput) => {
     let copy = [...toDoList];
     copy = [...copy, { id: Math.random(), task: userInput, complete: false }];
     setToDoList(copy);
-  }
+  };
 
-  const eraseLocalStorage = ()  => {
-    window.localStorage.clear();
-    document.location.reload()
-  }
+  const eraseLocalStorage = () => {
+    setToDoList([]);
+    window.localStorage.setItem("user", JSON.stringify([]));
+  };
   const saveLocalStorage = () => {
-    console.log(toDoList);
-    window.localStorage.setItem('user', JSON.stringify(toDoList));
+    window.localStorage.setItem("user", JSON.stringify(toDoList));
+  };
+  const handleChange = (e) => {
+    const result = toDoList.filter(
+      (element) => element.task == e.currentTarget.value
+    );
 
-  }
-  
+    if (result.length > 0) {
+      window.localStorage.setItem("user", JSON.stringify(toDoList));
+      setToDoList(result);
+      filtering = true;
+    } else {
+      if (filtering) {
+        setToDoList(JSON.parse(window.localStorage.getItem("user")));
+        filtering = false;
+      }
+    }
+  };
 
   return (
     <div className="App">
       <Header />
-      <ToDoList toDoList={toDoList} handleToggle={handleToggle} handleFilter={handleFilter}/>
-      <ToDoForm addTask={addTask}/>
-      
+      <ToDoList
+        toDoList={toDoList}
+        handleToggle={handleToggle}
+        handleFilter={handleFilter}
+      />
+      <ToDoForm addTask={addTask} />
+
+      <input
+        class="form-control"
+        type="text"
+        onChange={handleChange}
+        placeholder="Type to search..."
+      />
+
       <button class="btn btn-danger" onClick={eraseLocalStorage}>
         Erase local info and start over
       </button>
       <button class="btn btn-success" onClick={saveLocalStorage}>
         Save info on local storage
-      </button> 
+      </button>
     </div>
   );
- }
-
-  
+}
 
 export default App;
--- rampup/src/ToDoForm.js
+++ rampup/src/ToDoForm.js
@@ -1,24 +1,29 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 const ToDoForm = ({ addTask }) => {
+  const [userInput, setUserInput] = useState("");
 
-    const [ userInput, setUserInput ] = useState('');
+  const handleChange = (e) => {
+    setUserInput(e.currentTarget.value);
+  };
 
-    const handleChange = (e) => {
-        setUserInput(e.currentTarget.value)
-    }
-
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        addTask(userInput);
-        setUserInput("");
-    }
-    return (
-        <form onSubmit={handleSubmit}>
-            <input class="form-control" value={userInput} type="text" onChange={handleChange} placeholder="Enter task..."/>
-            <button class="btn btn-primary">Submit</button>
-        </form>
-    );
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    addTask(userInput);
+    setUserInput("");
+  };
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        class="form-control"
+        value={userInput}
+        type="text"
+        onChange={handleChange}
+        placeholder="Enter a task to add..."
+      />
+      <button class="btn btn-primary">Submit</button>
+    </form>
+  );
 };
 
 export default ToDoForm;
